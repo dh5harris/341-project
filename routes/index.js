1b566f4c7a7c4267378ef92fa2fd52d2b72e4306
dh5harris <dh5harris@hotmail.com>
2023-01-07 12:19:17 -0700

    added a test route

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,8 @@ const routes = require('express').Router();
 routes.get('/', (req, res) => {
   res.send('Kristy Harris');
 });
+routes.get('/test', (req, res) => {
+  res.send('Daniel Harris');
+});
 
 module.exports = routes;
